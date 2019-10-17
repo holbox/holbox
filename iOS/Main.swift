e238780a9a5792bc323e281ee23816f6ce54ca16
vauxhall <zero.griffin@gmail.com>
2019-10-17 13:17:20 +0200

    Add splash screen

--- iOS/Main.swift
+++ iOS/Main.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 final class Main: UIView {
+    private weak var logo: Logo?
+    
     required init?(coder: NSCoder) { nil }
     init() {
         super.init(frame: .zero)
@@ -8,8 +10,16 @@ final class Main: UIView {
         let logo = Logo()
         logo.start()
         addSubview(logo)
+        self.logo = logo
         
         logo.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         logo.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        loaded()
+    }
+    
+    func loaded() {
+        logo!.stop()
+        logo!.removeFromSuperview()
     }
 }
