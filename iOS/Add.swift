82a448e33809a464e14b54f3cd80927b034c4e20
vauxhall <zero.griffin@gmail.com>
2019-10-19 09:35:06 +0200

    Update detail

--- iOS/Add.swift
+++ iOS/Add.swift
@@ -6,6 +6,10 @@ final class Add: Modal {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let scroll = Scroll()
+        view.addSubview(scroll)
+        
         let icon = Image("new")
         
         let title = Label(.key("Add.title.\(app.mode.rawValue)"), 20, .bold, .white)
@@ -27,39 +31,47 @@ final class Add: Modal {
         let cancel = Control(.key("Add.cancel"), self, #selector(close), .clear, .init(white: 1, alpha: 0.4))
         
         [title, subtitle, info, _confirm, _purchases, cancel, icon, available].forEach {
-            view.addSubview($0)
-            $0.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            scroll.add($0)
+            $0.centerXAnchor.constraint(equalTo: scroll.centerX).isActive = true
         }
         
-        icon.bottomAnchor.constraint(equalTo: title.topAnchor, constant: -40).isActive = true
+        scroll.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scroll.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        scroll.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scroll.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        scroll.right.constraint(equalTo: view.rightAnchor).isActive = true
+        scroll.content.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: cancel.bottomAnchor, constant: 20).isActive = true
+        
+        icon.topAnchor.constraint(equalTo: scroll.content.safeAreaLayoutGuide.topAnchor, constant: 80).isActive = true
         icon.widthAnchor.constraint(equalToConstant: 60).isActive = true
         icon.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
-        title.bottomAnchor.constraint(equalTo: subtitle.topAnchor, constant: -10).isActive = true
+        title.topAnchor.constraint(equalTo: icon.bottomAnchor, constant: 30).isActive = true
         
-        subtitle.bottomAnchor.constraint(equalTo: available.topAnchor, constant: -50).isActive = true
+        subtitle.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 10).isActive = true
         
-        available.bottomAnchor.constraint(equalTo: info.topAnchor, constant: -50).isActive = true
+        available.topAnchor.constraint(equalTo: subtitle.bottomAnchor, constant: 20).isActive = true
         
-        info.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7).isActive = true
-        info.bottomAnchor.constraint(equalTo: _purchases.topAnchor, constant: -20).isActive = true
+        info.widthAnchor.constraint(equalTo: scroll.content.safeAreaLayoutGuide.widthAnchor, multiplier: 0.6).isActive = true
+        info.topAnchor.constraint(equalTo: available.bottomAnchor, constant: 20).isActive = true
         
-        _confirm.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7).isActive = true
-        _confirm.bottomAnchor.constraint(equalTo: cancel.topAnchor).isActive = true
+        _confirm.widthAnchor.constraint(equalTo: scroll.content.safeAreaLayoutGuide.widthAnchor, multiplier: 0.6).isActive = true
+        _confirm.topAnchor.constraint(equalTo: available.bottomAnchor, constant: 20).isActive = true
         
-        _purchases.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7).isActive = true
-        _purchases.bottomAnchor.constraint(equalTo: cancel.topAnchor).isActive = true
+        _purchases.widthAnchor.constraint(equalTo: scroll.content.safeAreaLayoutGuide.widthAnchor, multiplier: 0.6).isActive = true
+        _purchases.topAnchor.constraint(equalTo: info.bottomAnchor, constant: 20).isActive = true
         
-        cancel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7).isActive = true
-        cancel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        cancel.widthAnchor.constraint(equalTo: scroll.content.widthAnchor, multiplier: 0.6).isActive = true
         
         if app.session.available > 0 {
             info.isHidden = true
             _purchases.isHidden = true
             _purchases.target = nil
+            cancel.topAnchor.constraint(equalTo: _confirm.bottomAnchor, constant: 10).isActive = true
         } else {
             _confirm.isHidden = true
             _confirm.target = nil
+            cancel.topAnchor.constraint(equalTo: _purchases.bottomAnchor, constant: 10).isActive = true
         }
     }
     
