5aa5bff18fca829c2f5e45f81030499e5a7daeda
vauxhall <zero.griffin@gmail.com>
2019-10-31 15:18:42 +0100

    Update text

--- Shared/Storage.swift
+++ Shared/Storage.swift
@@ -10,6 +10,40 @@ final class Storage: NSTextStorage {
     override var string: String { storage.string }
     private let storage = NSTextStorage()
     
+    override func processEditing() {
+        super.processEditing()
+        storage.removeAttribute(.font, range: .init(location: 0, length: storage.length))
+        string.mark { (fonts[$0]!, .init($1, in: string)) }.forEach {
+            storage.addAttribute(.font, value: $0.0, range: $0.1)
+        }
+        edited(.editedAttributes, range: .init(location: 0, length: storage.length), changeInLength: 0)
+        layoutManagers.first!.processEditing(for: self, edited: .editedAttributes, range: .init(), changeInLength: 0, invalidatedRange: .init(location: 0, length: storage.length))
+    }
+    
+    override func addAttributes(_ attrs: [NSAttributedString.Key : Any] = [:], range: NSRange) {
+        storage.addAttributes(attrs, range: range)
+    }
+    
+    override func invalidateAttributes(in range: NSRange) {
+        storage.invalidateAttributes(in: range)
+    }
+    
+    override func enumerateAttribute(_ attrName: NSAttributedString.Key, in enumerationRange: NSRange, options: NSAttributedString.EnumerationOptions = [], using: (Any?, NSRange, UnsafeMutablePointer<ObjCBool>) -> Void) {
+        storage.enumerateAttribute(attrName, in: enumerationRange, options: options, using: using)
+    }
+    
+    override func attribute(_ attrName: NSAttributedString.Key, at: Int, effectiveRange: NSRangePointer?) -> Any? {
+        storage.attribute(attrName, at: at, effectiveRange: effectiveRange)
+    }
+    
+    override func attribute(_ attrName: NSAttributedString.Key, at: Int, longestEffectiveRange: NSRangePointer?, in rangeLimit: NSRange) -> Any? {
+        storage.attribute(attrName, at: at, longestEffectiveRange: longestEffectiveRange, in: rangeLimit)
+    }
+    
+    override func attributes(at location: Int, longestEffectiveRange: NSRangePointer?, in rangeLimit: NSRange) -> [NSAttributedString.Key : Any] {
+        storage.attributes(at: location, longestEffectiveRange: longestEffectiveRange, in: rangeLimit)
+    }
+    
     override func attributes(at: Int, effectiveRange: NSRangePointer?) -> [NSAttributedString.Key : Any] {
         storage.attributes(at: at, effectiveRange: effectiveRange)
     }
@@ -21,21 +55,16 @@ final class Storage: NSTextStorage {
     
     override func setAttributes(_ attrs: [NSAttributedString.Key : Any]?, range: NSRange) {
         storage.setAttributes(attrs, range: range)
+        edited(.editedAttributes, range: range, changeInLength: 0)
     }
     
     override func removeAttribute(_ name: NSAttributedString.Key, range: NSRange) {
         storage.removeAttribute(name, range: range)
+        edited(.editedAttributes, range: range, changeInLength: 0)
     }
     
     override func addAttribute(_ name: NSAttributedString.Key, value: Any, range: NSRange) {
         storage.addAttribute(name, value: value, range: range)
-    }
-    
-    override func processEditing() {
-        super.processEditing()
-        storage.removeAttribute(.font, range: .init(location: 0, length: storage.length))
-        string.mark { (fonts[$0]!, .init($1, in: string)) }.forEach {
-            storage.addAttribute(.font, value: $0.0, range: $0.1)
-        }
+        edited(.editedAttributes, range: range, changeInLength: 0)
     }
 }
