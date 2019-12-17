bd612e4c3b55cc535a53f63d036e8141a0984d05
vauxhall <zero.griffin@gmail.com>
2019-12-17 19:12:18 +0100

    Update timeline

--- Mac/Timeline.swift
+++ Mac/Timeline.swift
@@ -3,10 +3,18 @@ import AppKit
 final class Timeline: Chart {
     private weak var dots: CAShapeLayer!
     private weak var time: CAShapeLayer!
+    private weak var start: Label!
     
     required init?(coder: NSCoder) { nil }
     override init() {
         super.init()
+        
+        let now = Label(.key("Timeline.now"), 12, .medium, NSColor(named: "haze")!)
+        addSubview(now)
+        
+        let start = Label("", 12, .medium, NSColor(named: "haze")!)
+        addSubview(start)
+        self.start = start
         
         let dots = CAShapeLayer()
         dots.fillColor = .clear
@@ -27,7 +35,13 @@ final class Timeline: Chart {
         
         widthAnchor.constraint(greaterThanOrEqualToConstant: 100).isActive = true
         heightAnchor.constraint(greaterThanOrEqualToConstant: 100).isActive = true
-        heightAnchor.constraint(lessThanOrEqualToConstant: 300).isActive = true
+        heightAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
+        
+        start.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2).isActive = true
+        start.leftAnchor.constraint(equalTo: leftAnchor, constant: 3).isActive = true
+        
+        now.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2).isActive = true
+        now.rightAnchor.constraint(equalTo: rightAnchor, constant: -3).isActive = true
     }
     
     func refresh() {
@@ -35,7 +49,9 @@ final class Timeline: Chart {
         time.move(to: .init(x: 2, y: 30))
         
         let dates = (0 ..< app.session.cards(app.project, list: 2)).map { CGFloat(Int(app.session.content(app.project, list: 2, card: $0))!) }
-        if !dates.isEmpty {
+        if dates.isEmpty {
+            start.stringValue = ""
+        } else {
             let slots = (bounds.width - 60) / 3
             let interval = (.init(Date().timeIntervalSince1970) - dates.last!) / slots
             let tasks = (0 ... Int(slots)).reduce(into: [CGFloat]()) { tasks, slot in
@@ -44,7 +60,17 @@ final class Timeline: Chart {
             }
             let max = tasks.max()!
             tasks.enumerated().forEach {
-                time.addLine(to: .init(x: (((bounds.width - 60) / slots) * .init($0.0)) + 30, y: ((bounds.height - 60) * ($0.1 / max)) + 30))
+                time.addLine(to: .init(x: (((bounds.width - 40) / slots) * .init($0.0)) + 30, y: ((bounds.height - 60) * ($0.1 / max)) + 30))
+            }
+            
+            let date = Date(timeIntervalSince1970: .init(dates.last!))
+            if #available(OSX 10.15, *) {
+                start.stringValue = RelativeDateTimeFormatter().localizedString(for: date, relativeTo: .init())
+            } else {
+                let formatter = DateFormatter()
+                formatter.timeStyle = .short
+                formatter.dateStyle = Calendar.current.dateComponents([.day], from: date, to: .init()).day! == 0 ? .none : .short
+                start.stringValue = formatter.string(from: date)
             }
         }
         
